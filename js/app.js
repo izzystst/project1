6da018812f4f1261634f22e6b5b743fa0bd9710b
Izzy <IZZZZZY@Isobellas-MacBook-Pro-3.local>
2020-03-18 11:06:48 -0500

    made a dice roller

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 console.log('hell o')
+
+class Player {
+	constructor(name){
+		this.name = name
+		this.chips = 3
+	}
+}
+const game = {
+	//state
+	center: 0,
+
+
+	rollDice: function(){
+		let center = 1
+		let left = 2
+		let right = 3
+		let blank1 = 4
+		let blank2 = 5
+		let blank3 = 6
+		let roll = Math.ceil(Math.random()*6)
+		console.log(roll)
+	}
+
+}
